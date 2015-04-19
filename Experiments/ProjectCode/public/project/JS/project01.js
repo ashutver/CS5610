9ba815f10d5fa84e3ec84fb2e806f0d15ebbb660
ashutver <verma.a@husky.neu.edu>
2015-04-19 14:40:27 -0400

    project commit

--- Experiments/ProjectCode/public/project/JS/project01.js
+++ Experiments/ProjectCode/public/project/JS/project01.js
@@ -81,41 +81,7 @@ app.controller("navCntrl", function ($scope, $http, $location, $rootScope, Exerc
 });
 
 
-app.factory("ExerciseService", function ($http) {
-    var exercises = null;
-    var images = null;
-    var nutrients =[];
- /*   $http.get("http://wger.de/api/v2/exercise/?format=json&limit=170&language=2&callback=JSON_CALLBACK&ordering=name")
-     .success(function (response) {
-         exercises = response.results;
-         console.log("exercises length:"+exercises.length);
-     });*/
-    $http.get("https://wger.de/api/v2/exerciseimage/?limit=205&callback=JSON_CALLBACK")
-    .success(function (response) {
-        images = response.results;
-        console.log("images length:" + images.length);
-        //console.log(images);
-    });
-    $http.get("http://wger.de/api/v2/ingredient/?format=json&limit=8317&callback=JSON_CALLBACK")
-        .success(function (response) {
-            nutrients = response.results;
-        });
-           
-    var getExercises = function () {
-        return exercises;
-    }
-    var getImages = function () {
-        return images;
-    }
-    var getNutrients = function () {
-        return nutrients;
-    }
-    return {
-       // getExercises: getExercises,
-        getImages: getImages,
-        getNutrients: getNutrients
-    }
-})
+
 var checkLoggedin = function ($q, $timeout, $http, $location, $rootScope) {
     var deferred = $q.defer();
     $http.get('/api/loggedin').success(function (user) {
